24de475b7d45496811350cdf85346338c84a488c
carlos <carlos@loyal3.com>
2015-08-19 01:41:16 -0700

    Added lein nodejs and dependencies

--- infra/setup.sh
+++ infra/setup.sh
@@ -7,11 +7,6 @@ set -eu
 CLOSURE_BOOTSTRAP='https://raw.githubusercontent.com/google/closure-library/master/closure/goog/bootstrap/nodejs.js'
 
 
-# location of reactjs download
-#
-REACT_JS="http://fb.me/react-0.13.3.js"
-
-
 # where should these packages be installed to
 #
 BASEDIR="$(dirname $0)/../target/node"
@@ -28,23 +23,9 @@ if [ ! -f ${FILENAME} ] ; then
   (cd $(dirname ${FILENAME}); curl -s -o $(basename ${FILENAME}) ${CLOSURE_BOOTSTRAP})
 fi
 
-
-# download ReactJS
-#
-if [ ! -f ${BASEDIR}/react.js ] ; then
-  echo ""
-  echo "downloading reactjs"
-  curl -L -o ${BASEDIR}/react.js ${REACT_JS}
-fi
-
-
 # install express and other node modules
 #
-if [ ! -d ${BASEDIR}/node_modules ] ; then
-  echo ""
-  echo "installing express and other node modules"
-  (cd ${BASEDIR}; npm install express st restler)
-fi
+./infra/lein npm install
 
 
 # copy the server script file
